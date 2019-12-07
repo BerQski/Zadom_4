e339c1cb37f405ee684f1a2d62d038ba96c3d8f2
Bern <bernard.kulegowski@gmail.com>
2019-12-07 14:39:45 +0100

    echhh

--- Zadom_4.js
+++ Zadom_4.js
@@ -1,52 +1,88 @@
-// 1#
+// // 1#
 
-String.prototype.RevStr = function() {
-  return this.split("")
-    .reverse()
-    .join("");
-};
+// String.prototype.RevStr = function() {
+//   return this.split("")
+//     .reverse()
+//     .join("");
+// };
 
-let strToRev = "mozej utrot ad amad at ortuj ezom";
+// let strToRev = "mozej utrot ad amad at ortuj ezom";
 
-console.log(strToRev.RevStr());
+// console.log(strToRev.RevStr());
 
-// 2#
+// // 2#
 
-let numToRev = 1234567654321.34;
-Number.prototype.RevNum = function() {
-  return this.toString()
-    .split("")
-    .reverse()
-    .join("");
-};
+// let numToRev = 1234567654321.34;
+// Number.prototype.RevNum = function() {
+//   return this.toString()
+//     .split("")
+//     .reverse()
+//     .join("");
+// };
 
-console.log(numToRev.RevNum());
+// console.log(numToRev.RevNum());
 
 // 3#
+const setUrl = "./Data.json";
 
-const data = require("./Data.json");
-let jsDat = JSON.stringify(data);
-let jsDataEnt = JSON.parse(jsDat);
-console.log(jsDataEnt);
+function jsDataEnt(setUrl) {
+  const data = require(setUrl);
+  const str = JSON.stringify(data);
+  return JSON.parse(str);
+}
+console.log(jsDataEnt(setUrl));
 
-// {
-//     "index": 0,
-//     "_id": "5c01097e566bb68fe9611b25",
-//     "cost": "434.25",
-//     "detailsOfPayent": {
-//       "Type": 3,
-//       "company": "ECSTASIA",
-//       "date": "09-07-2014"
-//     }
-//   }
+const getDzejson = jsDataEnt(setUrl);
 
-// 3)	Based on included JSON file.
-// a.	Create a function that will return Json from the file. a
-// b.	Create a structures to hold data from the file. b
-// c.	Map data from function a to structure from b.
-// d.	Create object that will give us data about:
-// i.	How much money was spend in 2014
+function ObjInside(Type, company, date) {
+  this.Type = Type;
+  this.company = company;
+  this.date = new Date(date);
+}
+
+function ObjBig(id, _id, cost, Type, company, date) {
+  this.id = id;
+  this._id = _id;
+  this.cost = new Number(parseFloat(cost).toFixed(2));
+  this.detailsOfPayent = new ObjInside(Type, company, date);
+}
+
+let holderFolder = getDzejson.map(
+  d =>
+    new ObjBig(
+      d.index,
+      d._id,
+      d.cost,
+      d.detailsOfPayent.Type,
+      d.detailsOfPayent.company,
+      d.detailsOfPayent.date
+    )
+);
+console.log(holderFolder[0]);
+
+//i
+const in2014 = function(hf) {
+  return hf
+    .filter(fil => fil.detailsOfPayent.date.getFullYear() == 2014)
+    .reduce((accum, hf) => accum + hf.cost, 0);
+};
+
+console.log(in2014(holderFolder));
 // ii.	What company was earning how much
+
+const whatHow = function(hf, name) {
+  return hf
+    .filter(fil => fil.detailsOfPayent.company === name)
+    .reduce((accum, hf) => accum + hf.cost, 0);
+};
+
+console.log(whatHow(holderFolder, "MANGELICA"));
 // iii.	What type on transaction was having what spending’s.
-// iv.	Values of the spending in each month
-// v.	Values of the spending in each day of the week
+const findType = function(holderFolder) {
+  return holderFolder
+    .map(d => d.detailsOfPayent.Type)
+    .filter((d, i, holderFolder) => holderFolder.indexOf(d) === i);
+};
+
+console.log(findType(holderFolder));
+// What type on transaction was having what spending’s.
